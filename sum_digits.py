fe5552cf9aa12ed29e1233eda29268130aaeea0a
Yashodhar Govil <yash.govil@gmail.com>
2017-06-15 10:11:23 -0400

    fixed bugs with intaking cmd line args, finished exercise

--- sum_digits.py
+++ sum_digits.py
@@ -8,8 +8,8 @@ def sum_digits(int):
 	Funtion to sum the digits of the input of integer
 	'''
 	# create a string from the int
-	word = '' + int
-	
+	word = '' + str(int)
+
 	# find sum by referring to the index of digits string
 	sum = 0
 	for dig in word:
@@ -17,19 +17,19 @@ def sum_digits(int):
 	return sum
 
 # store first arg
-first = int(args[0])
+first = int(args[1])
 
 # if only one arg
-if len(args) == 1:
+if len(args) == 2:
 	# loop until you reach square of input
 	for x in range(1, first + 1):
 		print(sum_digits(x*first))
 
-# if 2 args 
-elif len(args) == 2:
-	for x in range(1, int(args[1]) + 1):
+# if 2 args
+elif len(args) == 3:
+	for x in range(1, int(args[2]) + 1):
 		if x%first==0:
-			print(sum_digits(x*first)) 
+			print(sum_digits(x))
 
 # if not 1 or 2 args, return error
 else:
